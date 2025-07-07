2186851d9946f30b6938d4e2cb6b8d25153bd93c
yourFluxpoint <fluxitegmd@gmail.com>
2025-07-07 13:09:42 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -41,7 +41,7 @@ export default {
                     <h1>{{ level.name }}</h1>
                     <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <p>{{ level.description }}</p>
-                    <z>{{ level.tags }}</z>
+                    <tagfont>{{ level.tags }}</tagfont>
                     <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
                     <ul class="stats">
                         <li>
